ed6fdbdf6cd4001d72d6235400fa745cdc9e7b60
Ubuntu <ubuntu@ip-172-31-53-78.ec2.internal>
2021-11-02 01:39:17 +0000

    FIX: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-r1 = Role.create!({ name: 'Buyer', description: 'Can read items' })
+r1 = Role.create!({ name: 'Buyer', description: 'Can read ' })
 r2 = Role.create!({ name: 'Organizer', description: 'Can read and create items. Can update and destroy own items' })
 r3 = Role.create!({ name: 'Admin', description: 'Can perform any CRUD operation on any resource' })
 
 u1 = User.create({ email: 'admin@admin.com', password: 'lala123', password_confirmation: 'lala123', role_id: r3.id })
-u1 = User.create({ email: 'evento@evento.com', password: 'lala123', password_confirmation: 'lala123', role_id: r2.id })
-u1 = User.create({ email: 'cliente@cliente.com', password: 'lala123', password_confirmation: 'lala123', role_id: r1.id })
+u2 = User.create({ email: 'evento@evento.com', password: 'lala123', password_confirmation: 'lala123', role_id: r2.id })
+u3 = User.create({ email: 'cliente@cliente.com', password: 'lala123', password_confirmation: 'lala123', role_id: r1.id })
